28d7ad3aa982cc1392609a96836110cd250a2d85
Andrew Cooks <acooks@linux.com>
2015-05-09 14:48:46 +0800

    change js order in concat file to reduce use-before-definition

--- frontend/output/js/jittertrap-concat.js
+++ frontend/output/js/jittertrap-concat.js
@@ -40,115 +40,6 @@ var websocket = {};
 var chart = {};
 var histogram = {};
 var basicStatsGraph = {};
-$(document).ready(function() {
-
-  var triggers = {
-    maxRxThroughputEnabled: false,
-    maxTxThroughputEnabled: false,
-    minRxThroughputEnabled: false,
-    minTxThroughputEnabled: false,
-    maxTxSilenceEnabled:    false,
-    maxRxSilenceEnabled:    false,
-    maxRxThroughputTriggerVal: 0,
-    maxTxThroughputTriggerVal: 0,
-    minRxThroughputTriggerVal: 0,
-    minTxThroughputTriggerVal: 0
-  };
-
-  // Initialize Chart Options
-  $("#chopts_dataLen").html(dataLength);
-  $("#chopts_chartPeriod").val(chartingPeriod);
-
-  // Initialize WebSockets
-  var wsUri = "ws://" + document.domain + ":" + location.port;
-  websocket = new WebSocket(wsUri);
-
-  websocket.onopen = function(evt) { 
-    websocket.send("open!");
-    list_ifaces();
-    get_sample_period();
-  };
-
-  websocket.onclose = function(evt) {
-    console.log("unhandled websocket onclose event: " + evt);
-  };
-
-  websocket.onerror = function(evt) {
-    console.log("unhandled websocket onerror event: " + evt);
-  };
-
-  websocket.onmessage = function(evt) {
-    var msg = JSON.parse(evt.data);
-    var selectedIface = $('#dev_select').val();
-
-    if (msg.stats && msg.stats.iface === selectedIface) {
-      var visibleSeries = $("#chopts_series option:selected").val();
-      handleMsgUpdateStats(samplePeriod, msg.stats.s, visibleSeries);
-    } else if (msg.ifaces) {
-      handleMsgIfaces(msg.ifaces);
-    } else if (msg.netem_params) {
-      handleMsgNetemParams(msg.netem_params);
-    } else if (msg.sample_period) {
-      handleMsgSamplePeriod(msg.sample_period);
-    }
-  };
-
-
-  // UI Event Handlers
-  $("#chopts_series").bind('change', resetChart);
-  $("#dev_select").bind('change', clearChart);
-  $('#set_netem_button').bind('click', set_netem);
-  $('#clear_netem_button').bind('click', clear_netem);
-  $('#dev_select').bind('change', dev_select);
-  $('#chopts_stop_start').bind('click', toggleStopStartGraph);
-
-  $("#chopts_chartPeriod").bind('change', function() {
-    chartingPeriod = $("#chopts_chartPeriod").val();
-    if (chartingPeriod < chartingPeriodMin) {
-       chartingPeriod = chartingPeriodMin;
-       $("#chopts_chartPeriod").val(chartingPeriod);
-    } else if (chartingPeriod > chartingPeriodMax) {
-       chartingPeriod = chartingPeriodMax;
-       $("#chopts_chartPeriod").val(chartingPeriod);
-    }
-
-    dataLength = Math.floor(dataLengthMultiplier * chartingPeriod);
-    $("#chopts_dataLen").html(dataLength);
-    resizeDataBufs(dataLength);
-    resetChart();
-  });
-
-  $('#more_chopts_toggle').click(function() {
-    $('#more_chopts').toggle("fast");
-    return false;
-  });
-
-  $('#trigger_toggle').click(function() {
-    $('#trigger_chopts').toggle("fast");
-    return false;
-  });
-
-  // Changing traps from the list of traps in the trap modal
-  $('#trap_names').bind('change', trapSelectionHandler);
-  // Add a trap
-  $('#add_trap_modal button').last().click(addTrapHandler);
-  // Remove trap button(s)
-  $('#traps_table tbody').on('click', 'tr button', function(event){
-    var $trapTr = $(event.target).parents('tr');
-
-    // Remove from JS
-    var trapId = $trapTr.data("trapId");
-    delete traps[trapId];
-
-    // Removal from the UI
-    $trapTr.remove();
-  });
-
-  $('#help_toggle').click(function() {
-    $('#help').toggle("fast");
-  });
-});
-
 /* count must be bytes, samplePeriod is microseconds */
 var byteCountToKbpsRate = function(count) {
   'use strict';
@@ -721,3 +612,112 @@ var addTrapHandler = function(event) {
     addTrapToUI();
   }
 };
+$(document).ready(function() {
+
+  var triggers = {
+    maxRxThroughputEnabled: false,
+    maxTxThroughputEnabled: false,
+    minRxThroughputEnabled: false,
+    minTxThroughputEnabled: false,
+    maxTxSilenceEnabled:    false,
+    maxRxSilenceEnabled:    false,
+    maxRxThroughputTriggerVal: 0,
+    maxTxThroughputTriggerVal: 0,
+    minRxThroughputTriggerVal: 0,
+    minTxThroughputTriggerVal: 0
+  };
+
+  // Initialize Chart Options
+  $("#chopts_dataLen").html(dataLength);
+  $("#chopts_chartPeriod").val(chartingPeriod);
+
+  // Initialize WebSockets
+  var wsUri = "ws://" + document.domain + ":" + location.port;
+  websocket = new WebSocket(wsUri);
+
+  websocket.onopen = function(evt) { 
+    websocket.send("open!");
+    list_ifaces();
+    get_sample_period();
+  };
+
+  websocket.onclose = function(evt) {
+    console.log("unhandled websocket onclose event: " + evt);
+  };
+
+  websocket.onerror = function(evt) {
+    console.log("unhandled websocket onerror event: " + evt);
+  };
+
+  websocket.onmessage = function(evt) {
+    var msg = JSON.parse(evt.data);
+    var selectedIface = $('#dev_select').val();
+
+    if (msg.stats && msg.stats.iface === selectedIface) {
+      var visibleSeries = $("#chopts_series option:selected").val();
+      handleMsgUpdateStats(samplePeriod, msg.stats.s, visibleSeries);
+    } else if (msg.ifaces) {
+      handleMsgIfaces(msg.ifaces);
+    } else if (msg.netem_params) {
+      handleMsgNetemParams(msg.netem_params);
+    } else if (msg.sample_period) {
+      handleMsgSamplePeriod(msg.sample_period);
+    }
+  };
+
+
+  // UI Event Handlers
+  $("#chopts_series").bind('change', resetChart);
+  $("#dev_select").bind('change', clearChart);
+  $('#set_netem_button').bind('click', set_netem);
+  $('#clear_netem_button').bind('click', clear_netem);
+  $('#dev_select').bind('change', dev_select);
+  $('#chopts_stop_start').bind('click', toggleStopStartGraph);
+
+  $("#chopts_chartPeriod").bind('change', function() {
+    chartingPeriod = $("#chopts_chartPeriod").val();
+    if (chartingPeriod < chartingPeriodMin) {
+       chartingPeriod = chartingPeriodMin;
+       $("#chopts_chartPeriod").val(chartingPeriod);
+    } else if (chartingPeriod > chartingPeriodMax) {
+       chartingPeriod = chartingPeriodMax;
+       $("#chopts_chartPeriod").val(chartingPeriod);
+    }
+
+    dataLength = Math.floor(dataLengthMultiplier * chartingPeriod);
+    $("#chopts_dataLen").html(dataLength);
+    resizeDataBufs(dataLength);
+    resetChart();
+  });
+
+  $('#more_chopts_toggle').click(function() {
+    $('#more_chopts').toggle("fast");
+    return false;
+  });
+
+  $('#trigger_toggle').click(function() {
+    $('#trigger_chopts').toggle("fast");
+    return false;
+  });
+
+  // Changing traps from the list of traps in the trap modal
+  $('#trap_names').bind('change', trapSelectionHandler);
+  // Add a trap
+  $('#add_trap_modal button').last().click(addTrapHandler);
+  // Remove trap button(s)
+  $('#traps_table tbody').on('click', 'tr button', function(event){
+    var $trapTr = $(event.target).parents('tr');
+
+    // Remove from JS
+    var trapId = $trapTr.data("trapId");
+    delete traps[trapId];
+
+    // Removal from the UI
+    $trapTr.remove();
+  });
+
+  $('#help_toggle').click(function() {
+    $('#help').toggle("fast");
+  });
+});
+
