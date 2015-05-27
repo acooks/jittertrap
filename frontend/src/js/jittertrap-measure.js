1b7743b7531fa37e4f37243a91876ba7548d9be0
Andrew Cooks <acooks@linux.com>
2015-05-27 19:35:42 +0800

    split large js function, because vanity metrics.

--- frontend/src/js/jittertrap-measure.js
+++ frontend/src/js/jittertrap-measure.js
@@ -16,8 +16,7 @@ JT = (function (my) {
   measurements.rxZRun.mean = "TODO";
   measurements.txZRun.mean = "TODO";
 
-  var updateDOM = function () {
-
+  var updateTputDOM = function () {
     $("#jt-measure-tput-min-rx").html(measurements.rxRate.min);
     $("#jt-measure-tput-max-rx").html(measurements.rxRate.max);
     $("#jt-measure-tput-mean-rx").html(measurements.rxRate.mean);
@@ -25,7 +24,9 @@ JT = (function (my) {
     $("#jt-measure-tput-min-tx").html(measurements.txRate.min);
     $("#jt-measure-tput-max-tx").html(measurements.txRate.max);
     $("#jt-measure-tput-mean-tx").html(measurements.txRate.mean);
+  };
 
+  var updateRateDOM = function () {
     $("#jt-measure-pktRate-min-rx").html(measurements.rxPacketRate.min);
     $("#jt-measure-pktRate-max-rx").html(measurements.rxPacketRate.max);
     $("#jt-measure-pktRate-mean-rx").html(measurements.rxPacketRate.mean);
@@ -33,13 +34,21 @@ JT = (function (my) {
     $("#jt-measure-pktRate-min-tx").html(measurements.txPacketRate.min);
     $("#jt-measure-pktRate-max-tx").html(measurements.txPacketRate.max);
     $("#jt-measure-pktRate-mean-tx").html(measurements.txPacketRate.mean);
+  };
 
+  var updateZRunDOM = function () {
     $("#jt-measure-zRun-max-rx").html(measurements.rxZRun.max);
     $("#jt-measure-zRun-mean-rx").html(measurements.rxZRun.mean);
 
     $("#jt-measure-zRun-max-rx").html(measurements.rxRate.maxZ);
     $("#jt-measure-zRun-max-tx").html(measurements.txRate.maxZ);
     $("#jt-measure-zRun-mean-tx").html(measurements.txZRun.mean);
+  };
+
+  var updateDOM = function () {
+    updateTputDOM();
+    updateRateDOM();
+    updateZRunDOM();
   };
 
   var drawIntervalID = setInterval(updateDOM, 100);
