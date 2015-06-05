febf312936d1accf980b28d0a2b6ba4f44c8e3b1
Andrew Cooks <acooks@linux.com>
2015-06-05 09:52:38 +0800

    move charting parameters from globals file to charting module

--- frontend/src/js/jittertrap-charting.js
+++ frontend/src/js/jittertrap-charting.js
@@ -6,6 +6,22 @@
 JT = (function (my) {
   'use strict';
 
+  my.charts = {};
+
+  /* Add a container for charting parameters */
+  my.charts.params = {};
+
+  /* time (milliseconds) represented by each data point */
+  my.charts.params.plotPeriod        = 60;
+  my.charts.params.plotPeriodMin     = 1;
+  my.charts.params.plotPeriodMax     = 500;
+
+  /* chart redraw/refresh/updates; milliseconds; 40ms == 25 Hz */
+  my.charts.params.redrawPeriod      = 60;
+  my.charts.params.redrawPeriodMin   = 40;
+  my.charts.params.redrawPeriodMax   = 100;
+  my.charts.params.redrawPeriodSaved = 0;
+
   var resetChart = function() {
     var selectedSeriesOpt = $("#chopts_series option:selected").val();
     var selectedSeries = my.core.series[selectedSeriesOpt];
--- frontend/src/js/jittertrap-globals.js
+++ frontend/src/js/jittertrap-globals.js
@@ -37,27 +37,4 @@ JT = (function (my) {
 }(JT));
 
 
-/*
- * ==== Charts ====
- */
-JT = (function (my) {
-  'use strict';
-  my.charts = {};
-
-  /* Add a container for charting parameters */
-  my.charts.params = {};
-
-  /* time (milliseconds) represented by each data point */
-  my.charts.params.plotPeriod        = 60;
-  my.charts.params.plotPeriodMin     = 1;
-  my.charts.params.plotPeriodMax     = 500;
-
-  /* chart redraw/refresh/updates; milliseconds; 40ms == 25 Hz */
-  my.charts.params.redrawPeriod      = 60;
-  my.charts.params.redrawPeriodMin   = 40;
-  my.charts.params.redrawPeriodMax   = 100;
-  my.charts.params.redrawPeriodSaved = 0;
-
-  return my;
-}(JT));
 /* end of jittertrap-globals.js */
