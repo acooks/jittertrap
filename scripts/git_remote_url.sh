e8b6c9682d9c219703609f2f20224b114598af69
Andrew Cooks <acooks@linux.com>
2015-07-24 16:14:57 +0800

    fix: escape repo url before regex replacement

--- scripts/git_remote_url.sh
+++ scripts/git_remote_url.sh
@@ -4,4 +4,4 @@ curr_branch=$(git rev-parse --abbrev-ref HEAD);
 curr_remote=$(git config branch.$curr_branch.remote || echo "unknown");
 remote_url=$(git ls-remote --get-url $curr_remote || echo "unknown");
 
-echo $remote_url
+echo $remote_url | sed -e 's/[\/&]/\\&/g'
