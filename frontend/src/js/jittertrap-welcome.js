fb43e970f2ecda79b875d847fc8d051d0e0c248e
Andrew Cooks <acooks@linux.com>
2015-07-24 16:18:59 +0800

    add a welcome message to the javascript sources.

---
+++ frontend/src/js/jittertrap-welcome.js
@@ -0,0 +1,12 @@
+/**
+ * Hi!
+ *
+ * You're reading the JitterTrap code?! That's great!
+ *
+ * Have a look at https://github.com/acooks/jittertrap
+ *
+ * Say 'hello'. We welcome your participation.
+ *
+ */
+
+
