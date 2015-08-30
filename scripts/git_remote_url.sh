d9a138a51be2aa4e22c14a8fc1a1e121912e0046
Andrew Cooks <acooks@linux.com>
2015-08-30 15:52:50 +0800

    give a hint about the purpose of the regex.

--- scripts/git_remote_url.sh
+++ scripts/git_remote_url.sh
@@ -4,4 +4,5 @@ curr_branch=$(git rev-parse --abbrev-ref HEAD);
 curr_remote=$(git config branch.$curr_branch.remote || echo "unknown");
 remote_url=$(git ls-remote --get-url $curr_remote || echo "unknown");
 
+# use sed to escape the / characters. "&" corresponds to the found pattern.
 echo $remote_url | sed -e 's/[\/&]/\\&/g'
