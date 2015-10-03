867119571f6f8692aba0f6326d7bf34a083d2258
Andrew Cooks <acooks@linux.com>
2015-10-03 17:13:23 +0800

    add a benchmark for the websocket mq

--- server/test_mq.c
+++ server/test_mq.c
@@ -1,3 +1,6 @@
+#define _POSIX_C_SOURCE 200809L
+#include <pthread.h>
+#include <time.h>
 #include <stdio.h>
 #include <assert.h>
 
@@ -6,8 +9,8 @@
 
 int message_producer(struct jt_ws_msg *m, void *data)
 {
-	char *s = (char *)data;
-	snprintf(m->m, MAX_JSON_MSG_LEN, "%s", s);
+	int *d = (int *)data;
+	sprintf(m->m, "message %d", *d);
 	return 0;
 }
 
@@ -30,6 +33,50 @@ int string_copier(struct jt_ws_msg *m, void *data)
 	sprintf(s, m->m);
 	return 0;
 }
+
+int benchmark_produce(struct jt_ws_msg *m, void *data __attribute__((unused)))
+{
+	m->m[0] = '\0';
+	return 0;
+}
+
+int benchmark_consumer(struct jt_ws_msg *m __attribute__((unused)), void *data __attribute__((unused)))
+{
+	return 0;
+}
+
+/* Calculate the absolute difference between t1 and t2. */
+struct timespec ts_absdiff(struct timespec t1, struct timespec t2)
+{
+        struct timespec t;
+        if ((t1.tv_sec < t2.tv_sec) ||
+            ((t1.tv_sec == t2.tv_sec) && (t1.tv_nsec <= t2.tv_nsec))) {
+                /* t1 <= t2 */
+                t.tv_sec = t2.tv_sec - t1.tv_sec;
+                if (t2.tv_nsec < t1.tv_nsec) {
+                        t.tv_nsec = t2.tv_nsec + 1E9 - t1.tv_nsec;
+                        t.tv_sec--;
+                } else {
+                        t.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+                }
+        } else {
+                /* t1 > t2 */
+                t.tv_sec = t1.tv_sec - t2.tv_sec;
+                if (t1.tv_nsec < t2.tv_nsec) {
+                        t.tv_nsec = t1.tv_nsec + 1E9 - t2.tv_nsec;
+                        t.tv_sec--; /* Borrow a second. */
+                } else {
+                        t.tv_nsec = t1.tv_nsec - t2.tv_nsec;
+                }
+        }
+        return t;
+}
+
+double ts_to_seconds(struct timespec t)
+{
+        return t.tv_sec + t.tv_nsec / 1E9;
+}
+
 
 int test_consume_from_empty()
 {
@@ -61,7 +108,6 @@ int test_produce_overflow()
 {
 	int i, err;
 	unsigned long id;
-	char msg[MAX_JSON_MSG_LEN];
 
 	printf("test for produce overflow handling.\n");
 
@@ -72,13 +118,11 @@ int test_produce_overflow()
 	assert(!err);
 
 	for (i = 0; i < MAX_Q_DEPTH - 1; i++) {
-		snprintf(msg, MAX_JSON_MSG_LEN, "Message %d", i);
-		printf("msg: %s\n", msg);
-		err = jt_ws_mq_produce(message_producer, msg);
+		err = jt_ws_mq_produce(message_producer, &i);
 		assert(!err);
 	}
-	printf("queue full: %d\n", i);
-	err = jt_ws_mq_produce(message_producer, msg);
+	printf("queue full: %d messages\n", i+1);
+	err = jt_ws_mq_produce(message_producer, &i);
 	assert(err);
 
 	err = jt_ws_mq_consumer_unsubscribe(id);
@@ -108,8 +152,7 @@ int test_produce_consume()
 	/* fill up the queue */
 	i = 0;
 	do {
-		snprintf(s, MAX_JSON_MSG_LEN, "message number %d", i);
-		err = jt_ws_mq_produce(message_producer, s);
+		err = jt_ws_mq_produce(message_producer, &i);
 		if (!err) {
 			i++;
 		}
@@ -141,6 +184,7 @@ int test_ppcc()
 	int err;
 	unsigned long id;
 	char s[MAX_JSON_MSG_LEN];
+	int msg_id;
 
 	printf("Testing PPCC case\n");
 
@@ -150,10 +194,12 @@ int test_ppcc()
 	err = jt_ws_mq_consumer_subscribe(&id);
 	assert(!err);
 
-	err = jt_ws_mq_produce(message_producer, "Message 1");
+	msg_id = 1;
+	err = jt_ws_mq_produce(message_producer, &msg_id);
 	assert(!err);
 
-	err = jt_ws_mq_produce(message_producer, "Message 2");
+	msg_id = 2;
+	err = jt_ws_mq_produce(message_producer, &msg_id);
 	assert(!err);
 
 	err = jt_ws_mq_consume(id, string_copier, s);
@@ -183,6 +229,7 @@ int test_pcpc()
 	int err;
 	unsigned long id;
 	char s[MAX_JSON_MSG_LEN];
+	int msg_id;
 
 	printf("Testing PCPC case\n");
 
@@ -192,13 +239,15 @@ int test_pcpc()
 	err = jt_ws_mq_consumer_subscribe(&id);
 	assert(!err);
 
-	err = jt_ws_mq_produce(message_producer, "message number 1");
+	msg_id = 1;
+	err = jt_ws_mq_produce(message_producer, &msg_id);
 	assert(!err);
 
 	err = jt_ws_mq_consume(id, message_printer, s);
 	assert(!err);
 
-	err = jt_ws_mq_produce(message_producer, "message number 2");
+	msg_id = 2;
+	err = jt_ws_mq_produce(message_producer, &msg_id);
 	assert(!err);
 
 	err = jt_ws_mq_consume(id, message_printer, s);
@@ -223,6 +272,7 @@ int test_pccpcc()
 	int err;
 	unsigned long id;
 	char s[MAX_JSON_MSG_LEN];
+	int msg_id;
 
 	printf("Testing PCCP case\n");
 
@@ -231,7 +281,8 @@ int test_pccpcc()
 
 	err = jt_ws_mq_consumer_subscribe(&id);
 
-	err = jt_ws_mq_produce(message_producer, "message number 1");
+	msg_id = 1;
+	err = jt_ws_mq_produce(message_producer, &msg_id);
 	assert(!err);
 
 	err = jt_ws_mq_consume(id, message_printer, s);
@@ -241,7 +292,8 @@ int test_pccpcc()
 	err = jt_ws_mq_consume(id, message_printer, s);
 	assert(err);
 
-	err = jt_ws_mq_produce(message_producer, "message number 2");
+	msg_id = 2;
+	err = jt_ws_mq_produce(message_producer, &msg_id);
 	assert(!err);
 
 	err = jt_ws_mq_consume(id, message_printer, s);
@@ -261,6 +313,65 @@ int test_pccpcc()
 	return 0;
 }
 
+int benchmark()
+{
+	int i,j, err;
+	unsigned long id;
+	char s[MAX_JSON_MSG_LEN];
+	struct timespec start;
+	struct timespec end;
+	struct timespec diff;
+	double rate;
+
+#define TEST_ITERATIONS 100000
+
+	printf("Benchmarking... %d iterations \n", TEST_ITERATIONS);
+
+	err = jt_ws_mq_init();
+	assert(!err);
+
+	err = jt_ws_mq_consumer_subscribe(&id);
+	assert(!err);
+
+	clock_gettime(CLOCK_MONOTONIC, &start);
+
+	j = TEST_ITERATIONS;
+	while (j--) {
+
+		/* fill up the queue */
+		i = 0;
+		do {
+			int msg_id = j * i;
+			err = jt_ws_mq_produce(benchmark_produce, &msg_id);
+			if (!err) {
+				i++;
+			}
+		} while (!err);
+
+		/* we hava a full message queue, now consume it all */
+		for (; i > 0; i--) {
+			err = jt_ws_mq_consume(id, benchmark_consumer, s);
+			assert(!err);
+		}
+	}
+	clock_gettime(CLOCK_MONOTONIC, &end);
+
+	diff = ts_absdiff(start, end);
+	rate = TEST_ITERATIONS / ts_to_seconds(diff);
+
+	printf("Rate: %f msgs/s\n", rate);
+
+	err = jt_ws_mq_consumer_unsubscribe(id);
+	assert(!err);
+
+	err = jt_ws_mq_destroy();
+	assert(!err);
+
+	printf("OK.\n");
+	return 0;
+}
+
+
 int main()
 {
 	assert(0 == test_consume_from_empty());
@@ -269,6 +380,8 @@ int main()
 	assert(0 == test_ppcc());
 	assert(0 == test_pcpc());
 	assert(0 == test_pccpcc());
+	assert(0 == benchmark());
+
 	printf("message queue tests passed.\n");
 	return 0;
 }
