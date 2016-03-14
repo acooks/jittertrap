453e93707769a0ad3874bed887ad192a5dd63120
Andrew Cooks <acooks@linux.com>
2016-03-14 18:50:44 +0800

    basic play, stop, edit program functionality

--- html5-client/src/js/jittertrap-programs.js
+++ html5-client/src/js/jittertrap-programs.js
@@ -10,34 +10,92 @@ JT = (function (my) {
 
   var programs = {};
   var nextPID = 123;
+  var runningProgram = {};
 
   var Program = function (json) {
     this.id = "program_" + nextPID++;
     this.name = json.name;
-    console.log("name: "+json.name);
-    programs[this.id] = this;
-  };
+    this.timeoutHandles = {};
+    this.impairments = json.impairments;
 
-  my.programsModule.addProgramHandler = function(event) {
-    var pgm = $("#new_program").val();
-    console.log("new program:" + pgm);
-    loadProgram(pgm);
+    programs[this.id] = this;
+
+    this.play = function() {
+      console.log("playing program: " + this.id);
+      if (runningProgram && runningProgram.stop) {
+        runningProgram.stop();
+      }
+      JT.ws.clear_netem();
+      runningProgram = this;
+      $("#"+this.id+"_play").css('color','green');
+
+      for (var i in this.impairments) {
+        if (json.impairments[i].stop) {
+          console.log("stop at " + i);
+          this.timeoutHandles[i] = setTimeout(function() {
+              $("#delay").val(0);
+              $("#jitter").val(0);
+              $("#loss").val(0);
+              JT.ws.set_netem();
+              runningProgram.stop();
+            },
+            i * 1000
+          );
+        } else {
+          this.timeoutHandles[i] = setTimeout(function(t, d, j, l) {
+              console.log(Date.now() + " t = " + t + ", d: " + d + ", j: " + j + ", l:" + l);
+              $("#delay").val(d);
+              $("#jitter").val(j);
+              $("#loss").val(l);
+              JT.ws.set_netem();
+            },
+            i*1000,
+            i,
+            runningProgram.impairments[i].delay,
+            runningProgram.impairments[i].jitter,
+            runningProgram.impairments[i].loss
+          );
+        }
+      }
+    };
+
+    this.stop = function() {
+      console.log("stopping program: " + this.id);
+      if (this != runningProgram) {
+        return runningProgram.stop();
+      }
+      for (var th in this.timeoutHandles) {
+        clearTimeout(this.timeoutHandles[th]);
+      }
+      JT.ws.clear_netem();
+      runningProgram = null;
+      $("#"+this.id+"_play").css('color','#333');
+    };
   };
 
   my.programsModule.templateProgram = JSON.stringify(
     {
-      id: "program_123",
       name: "templateProgram",
       traps: [],
       impairments: {
-        1: { delay: 10, jitter: 2, loss: 0,   trapid: 0},
-        2: { delay: 15, jitter: 2, loss: 0.1, trapid: 0},
-        3: { delay: 0,  jitter: 0, loss: 0,   trapid: 1},
+        0: { delay: 0,  jitter: 0, loss: 0,   trapid: 0},
+        5: { delay: 10, jitter: 2, loss: 0,   trapid: 0},
+        10: { delay: 15, jitter: 2, loss: 0.1, trapid: 0},
+        15: { delay: 0,  jitter: 0, loss: 0,   trapid: 1},
+        20: { stop: true },
       }
     },
     null,
     '\t'
   );
+
+  my.programsModule.addProgramHandler = function(event) {
+    var pgm_txt = $("#new_program").val();
+    console.log("new program:" + pgm_txt);
+
+    var pgm = new Program(JSON.parse(pgm_txt));
+    updateUI(pgm);
+  };
 
   var updateUI = function(pgm) {
     var programTable = $('#programs_table');
@@ -48,9 +106,22 @@ JT = (function (my) {
                           },
           rendered      = Mustache.render(template, template_data);
 
-      console.log(rendered);
-
       programTable.find('tbody').append(rendered);
+
+      // Play button
+      $("#"+pgm.id+"_play").on('click', function(event) {
+        pgm.play();
+      });
+
+      // Stop button
+      $("#"+pgm.id+"_stop").on('click', function(event) {
+        pgm.stop();
+      });
+
+      // Edit button
+      $("#"+pgm.id+"_edit").on('click', function(event) {
+        $("#new_program").val(JSON.stringify(pgm, null, '\t'));
+      });
 
       // Remove button
       $("#"+pgm.id+"_delete").on('click', function(event) {
@@ -62,19 +133,10 @@ JT = (function (my) {
         tr.remove();
       });
 
-      // Reset button
-      $("#"+pgm.id+"_reset").on('click', function(event) {
-        pgm.reset();
-      });
     });
 
+    // Close the modal dialog
     $('#add_program_modal button').get(1).click();
-  };
-
-  var loadProgram = function(pgm_txt) {
-    var pgm = new Program(JSON.parse(pgm_txt));
-    updateUI(pgm);
-    console.log(programs);
   };
 
   return my;
