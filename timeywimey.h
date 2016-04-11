015ba758a9530ccf28232c7746127a4d8edfe99d
Andrew Cooks <acooks@linux.com>
2016-04-11 21:03:56 +0800

    add time diff module

---
+++ timeywimey.h
@@ -0,0 +1,8 @@
+#ifndef TIMEYWIMEY_H
+#define TIMEYWIMEY_H
+
+/* Calculate the absolute difference between t1 and t2. */
+struct timespec ts_absdiff(struct timespec t1, struct timespec t2);
+struct timeval tv_absdiff(struct timeval t1, struct timeval t2);
+struct timespec ts_add(struct timespec t1, struct timespec t2);
+#endif
