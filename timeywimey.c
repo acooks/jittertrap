f34dba40b01121a1e8e5cf43bff20d20609bf3db
Andrew Cooks <acooks@linux.com>
2016-04-21 12:45:39 +0800

    add timespec comparator function

--- timeywimey.c
+++ timeywimey.c
@@ -68,6 +68,19 @@ int tv_cmp(struct timeval t1, struct timeval t2)
 	return 0;
 }
 
+int ts_cmp(struct timespec t1, struct timespec t2)
+{
+	if ((t1.tv_sec < t2.tv_sec) ||
+	    ((t1.tv_sec == t2.tv_sec) && (t1.tv_nsec < t2.tv_nsec))) {
+		return -1;
+	} else if ((t1.tv_sec > t2.tv_sec) ||
+	           ((t1.tv_sec == t2.tv_sec) && (t1.tv_nsec > t2.tv_nsec))) {
+		return 1;
+	}
+	return 0;
+}
+
+
 inline struct timespec ts_add(struct timespec t1, struct timespec t2)
 {
 	struct timespec t;
