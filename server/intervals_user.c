9c4ee4a081051875f2f99a2b52820b6bcdcdb0e8
Andrew Cooks <acooks@linux.com>
2016-07-24 17:52:56 +1000

    add intervals_user.{c,h} to setup the toptalk library

---
+++ server/intervals_user.c
@@ -0,0 +1,15 @@
+#include <sys/time.h>
+
+#include "intervals_user.h"
+
+struct timeval const tt_intervals[INTERVAL_COUNT] = {
+        { .tv_sec = 0,  .tv_usec = 1E5 },
+        { .tv_sec = 0,  .tv_usec = 2E5 },
+        { .tv_sec = 0,  .tv_usec = 5E5 },
+        { .tv_sec = 1,  .tv_usec = 0 },
+        { .tv_sec = 3,  .tv_usec = 0 },
+        { .tv_sec = 5,  .tv_usec = 0 },
+        { .tv_sec = 10, .tv_usec = 0 },
+        { .tv_sec = 60, .tv_usec = 0 }
+};
+
