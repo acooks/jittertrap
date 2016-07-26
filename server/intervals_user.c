56059c3d4361532d95d97c6611b3c0a4fad03424
Andrew Cooks <acooks@linux.com>
2016-07-26 19:58:42 +1000

    let the toptalk intervals match the existing jittertrap intervals

--- server/intervals_user.c
+++ server/intervals_user.c
@@ -3,13 +3,11 @@
 #include "intervals_user.h"
 
 struct timeval const tt_intervals[INTERVAL_COUNT] = {
+        { .tv_sec = 0,  .tv_usec = 5E3 },
+        { .tv_sec = 0,  .tv_usec = 1E4 },
+        { .tv_sec = 0,  .tv_usec = 2E4 },
+        { .tv_sec = 0,  .tv_usec = 5E4 },
         { .tv_sec = 0,  .tv_usec = 1E5 },
-        { .tv_sec = 0,  .tv_usec = 2E5 },
-        { .tv_sec = 0,  .tv_usec = 5E5 },
-        { .tv_sec = 1,  .tv_usec = 0 },
-        { .tv_sec = 3,  .tv_usec = 0 },
-        { .tv_sec = 5,  .tv_usec = 0 },
-        { .tv_sec = 10, .tv_usec = 0 },
-        { .tv_sec = 60, .tv_usec = 0 }
+        { .tv_sec = 0,  .tv_usec = 2E5 }
 };
 
