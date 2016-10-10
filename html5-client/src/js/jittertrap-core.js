16dac12a7941c106a3800e69210ec835faf2ca6b
Andrew Cooks <acooks@linux.com>
2016-10-11 07:42:58 +1000

    fix chartPeriod == interval checking
    chartPeriod is a string; interval is a number. Conversions are fragile.

--- html5-client/src/js/jittertrap-core.js
+++ html5-client/src/js/jittertrap-core.js
@@ -213,7 +213,7 @@ JT = (function (my) {
     updateSampleCounts(interval);
 
     // careful, chartPeriod is a string. interval is in ns, so convert to ms.
-    if (chartPeriod !== (interval / 1E6)) {
+    if (Number(chartPeriod) !== Number(interval / 1E6)) {
       // only update chart if selected chartPeriod matches new data message
       return;
     }
