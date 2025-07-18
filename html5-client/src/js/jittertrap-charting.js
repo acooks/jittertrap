185165146acfb10056361521e92c899edea270fc
Andrew Cooks <acooks@gmail.com>
2025-07-18 20:24:11 +1000

    use requestAnimationFrame for smoother animations
    Decoupled the Render Loop for "modern, efficient rendering architecture" by
    replacing setInterval with requestAnimationFrame.
    This results in smoother animations, by synchronizing with the browser's
    repaint cycle instead of the data arrival time.

--- html5-client/src/js/jittertrap-charting.js
+++ html5-client/src/js/jittertrap-charting.js
@@ -11,6 +11,9 @@ JT = (function (my) {
   /* Add a container for charting parameters */
   var params = {};
 
+  /* Dirty flag signals when a redraw is needed. */
+  var isDirty = true;
+
   /* time (milliseconds) represented by each point on the chart */
   params.plotPeriod        = 100;
   params.plotPeriodMin     = 1;
@@ -76,15 +79,18 @@ JT = (function (my) {
   var renderCount = 0;
   var renderTime = 0;
 
-  var tuneChartUpdatePeriod = function() {
-
-    var tuneWindowSize = 5; // how often to adjust the updatePeriod.
-
-    if ((renderCount % tuneWindowSize) !== 0) {
+  var renderGraphs = function() {
+    /* Only redraw if the data has changed */
+    if (!isDirty) {
       return;
     }
 
-    var avgRenderTime =  Math.floor(renderTime / renderCount);
+    my.charts.tput.tputChart.redraw();
+    my.charts.pgaps.packetGapChart.redraw();
+    my.charts.toptalk.toptalkChart.redraw();
+
+    isDirty = false; // Clear the dirty flag after drawing
+    var tuneWindowSize = 5; // how often to adjust the updatePeriod.
 
     params.redrawPeriod = params.plotPeriod / 2;
 
@@ -94,45 +100,32 @@ JT = (function (my) {
       params.redrawPeriod = params.redrawPeriodMax;
     }
 
-    setUpdatePeriod();
-
-    renderCount = renderCount % tuneWindowSize;
     renderTime = 0;
   };
 
+  var drawIntervalID;
 
-  var renderGraphs = function() {
-    var d1 = Date.now();
-    my.charts.tput.tputChart.redraw();
-    my.charts.pgaps.packetGapChart.redraw();
-    my.charts.toptalk.toptalkChart.redraw();
+  var animationLoop = function() {
+    renderGraphs();
+    drawIntervalID = requestAnimationFrame(animationLoop);
+  }
 
-    var d2 = Date.now();
-    renderCount++;
-    renderTime += d2 - d1;
-    tuneChartUpdatePeriod();
-  };
-
-  var setUpdatePeriod = function() {
-    var updateRate = 1000.0 / params.redrawPeriod; /* Hz */
-    clearInterval(drawIntervalID);
-    drawIntervalID = setInterval(renderGraphs, params.redrawPeriod);
-  };
-
-  var toggleStopStartGraph = function() {
-    var maxUpdatePeriod = 9999999999;
-    if (params.redrawPeriod !== maxUpdatePeriod) {
-      params.redrawPeriodSaved = params.redrawPeriod;
-      params.redrawPeriod = maxUpdatePeriod;
-    } else {
-      params.redrawPeriod = params.redrawPeriodSaved;
-    }
-    setUpdatePeriod();
-    return false;
+  my.charts.setDirty = function() {
+    isDirty = true;
   };
 
   var getChartPeriod = function () {
     return params.plotPeriod;
+  };
+
+
+  var toggleStopStartGraph = function() {
+    if (drawIntervalID) {
+      cancelAnimationFrame(drawIntervalID);
+      drawIntervalID = 0;
+    } else {
+      drawIntervalID = requestAnimationFrame(animationLoop);
+    }
   };
 
   var setChartPeriod = function (newPeriod) {
@@ -150,17 +143,14 @@ JT = (function (my) {
     return {newPeriod: newPeriod, sampleCount: sampleCount};
   };
 
-  var drawIntervalID;
-
   /* Initialize all charts and start the render loop */
   my.charts.init = function() {
     resetChart();
-    drawIntervalID = setInterval(renderGraphs, params.redrawPeriod);
+    drawIntervalID = requestAnimationFrame(animationLoop);
   };
 
   /* Export "public" functions */
   my.charts.toggleStopStartGraph = toggleStopStartGraph;
-  my.charts.setUpdatePeriod = setUpdatePeriod;
   my.charts.getChartPeriod = getChartPeriod;
   my.charts.setChartPeriod = setChartPeriod;
   my.charts.resetChart = resetChart;
